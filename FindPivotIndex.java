d2d8de6968c64f4ea11b680bfdbdda3cbedea51b
NITESH3000 <nitesh.sharmaaz30@gmail.com>
2024-07-29 14:57:41 +0530

    FindPivotIndex

---
+++ FindPivotIndex.java
@@ -0,0 +1,34 @@
+
+
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        int i = 1;
+       
+        int rightSum = 0;
+        int leftSum = 0;
+        int sum = 0;
+        for(int num:nums){
+            sum+=num;
+        }
+        if(sum-nums[0] == 0){
+            return 0;
+        }
+        while(i<nums.length){
+            leftSum +=nums[i-1];
+            rightSum = sum -leftSum-nums[i];
+            if(leftSum == rightSum){
+                return i;
+            }
+            i++;
+            
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        FindPivotIndex find = new FindPivotIndex();
+        int[] nums ={1,2,3};
+        // int i = 0;
+        // while(i++ != 10000000)
+        System.out.println(find.pivotIndex(nums));
+    }
+}
